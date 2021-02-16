3fab0877fb178c13095c5baf348dfd66b752c40d
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2021-02-16 17:55:02 -0500

    initial checkin

---
+++ lapack/dgemm_acc.F90
@@ -0,0 +1,59 @@
+      subroutine dgemm(transA,transB,m,n,kk,                              &
+     & alpha, A,lda, B,ldb, beta, C,ldc )
+!$acc routine vector
+      implicit none
+      character, intent(in) :: transA,transB
+      integer, intent(in) :: m,n,kk,lda,ldb,ldc
+      real*8, intent(in) :: alpha,beta
+      real*8, intent(in) :: A(lda,*), B(ldb,*)
+      real*8, intent(inout) :: C(ldc,*)
+
+      integer, parameter :: nb = 64
+      integer :: i,j,k
+      integer :: istart,iend, jstart,jend
+      real*8 :: cij,aik,bkj
+      logical :: is_transA, is_transB
+
+      is_transA = (transA.eq.'T').or.(transA.eq.'t')
+      is_transB = (transB.eq.'T').or.(transB.eq.'t')
+!$acc loop vector collapse(2)
+      do j=1,n
+      do i=1,m
+        if (beta.eq.0) then
+                C(i,j) = 0
+        else
+                C(i,j) = beta * C(i,j)
+        endif
+      enddo
+      enddo
+
+      do jstart=1,n,nb
+      do istart=1,m,nb
+         jend = min(n,jstart+nb-1)
+         iend = min(m,istart+nb-1)
+!$acc loop vector collapse(2) private(cij,aik,bkj,k)
+      do j=jstart,jend
+      do i=istart,iend
+        cij = 0
+        do k=1,kk
+          if (is_transA) then
+                  aik = A(k,i)
+          else
+                  aik = A(i,k)
+          endif
+          if (is_transB) then
+                  bkj = B(j,k)
+          else
+                  bkj = B(k,j)
+          endif
+          cij = cij + aik*bkj
+        enddo
+        C(i,j) = C(i,j) + alpha*cij
+      enddo
+      enddo
+
+      enddo
+      enddo
+
+      return
+      end subroutine dgemm
