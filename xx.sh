05d11c0225ef237c10470b86dcd61bf68a1ae2a5
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2021-02-17 14:52:37 -0500

    simple script

--- xx.sh
+++ xx.sh
@@ -1,2 +1,2 @@
-pgf90 -Minfo=all -c -mp -acc  -ta=multicore -Mfixed lapack_acc.F90 
-pgf90  -Minfo=all -mp -acc -ta=multicore lapack_acc.o test1.F90 
+nvfortran -acc -Minfo=all -gpu=cc60  -Mfixed -c lapack_acc.F90
+
