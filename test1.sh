ed434d3d830ca84655ba1f6cec906b65e49e0179
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2021-02-17 14:49:27 -0500

    update, need -Minline=size:99

--- test1.sh
+++ test1.sh
@@ -1,2 +1,2 @@
 pgf90 -Minfo=all -c -acc -Mcuda -ta=tesla,cc60,managed,keepgpu -Mfixed lapack_acc.F90 
-pgf90  -Minfo=all -mp -acc -Mcuda -ta=tesla,cc60,managed,keepgpu lapack_acc.o test1.F90 
+pgf90  -Minfo=all -mp -acc -Mcuda -ta=tesla,cc60,managed,keepgpu lapack_acc.o -o test1 test1.F90  -llapack -lblas
--- test2.sh
+++ test2.sh
@@ -1,3 +1,3 @@
 
-pgf90 -Minfo=all -c -acc:verystrict -Mcuda -ta=tesla,cc60,managed -Mfixed lapack_acc.F90 
-pgf90  -o test2 -Minfo=all -mp -acc:verystrict -Mcuda -ta=tesla,cc60,managed lapack_acc.o test2.F90  -lopenblas
+pgf90 -Minline=size:999,reshape -UUSE_MANAGED -Minfo=all -c -acc -Mcuda -ta=tesla,cc60,managed -Mfixed lapack_acc.F90 
+pgf90  -o test2 -Minfo=all -mp -acc -Mcuda -ta=tesla,cc60,managed lapack_mod.F90 lapack_acc.o test2.F90  -llapack -lblas
