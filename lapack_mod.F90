80d8b8c5888ca596c93a2dcffc6fef5d3a680caa
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-02-17 20:59:36 -0500

    add routines for banded matrices

--- lapack_mod.F90
+++ lapack_mod.F90
@@ -12,4 +12,27 @@
       end subroutine dgemm
       end interface
 
+
+
+      interface
+      subroutine dgbmv(trans,m,n,kl,ku,alpha,A,lda,                       &
+     &                 x,incx,beta,y,incy)
+      implicit none
+      character trans
+      integer m,n,kl,ku,lda,incx,incy
+      real*8 alpha,beta,x(*),y(*)
+      real*8 A(lda,*)
+      end subroutine dgbmv
+
+      subroutine dgbsv(n,kl,ku,nrhs,AB,ldab,ipiv,B,ldb,info)
+      implicit none
+      integer n,kl,ku,nrhs,ldab,ldb
+      integer ipiv(*)
+      integer info
+      real*8 AB(ldab,*)
+      real*8 B(ldb,*)
+      end subroutine dgbsv
+
+      end interface
+
       end module lapack_mod
