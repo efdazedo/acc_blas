656979f763a1d81ae95669fa5296127c90c0193c
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2021-02-20 15:42:03 -0500

    leave out cc60 or cc70

--- test1.sh
+++ test1.sh
@@ -1,2 +1,2 @@
-pgf90 -Minfo=all -c -acc -Mcuda -ta=tesla,cc60,managed,keepgpu -Mfixed lapack_acc.F90 
-pgf90  -Minfo=all -mp -acc -Mcuda -ta=tesla,cc60,managed,keepgpu lapack_acc.o -o test1 test1.F90  -llapack -lblas
+pgf90 -Minfo=all -c -acc -Mcuda -ta=tesla,managed,keepgpu -Mfixed lapack_acc.F90 
+pgf90  -Minfo=all -mp -acc -Mcuda -ta=tesla,managed,keepgpu lapack_acc.o -o test1 test1.F90  -llapack -lblas
