6fb572c4cc719fb2ef97adfc2794d0dd79f6aafe
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-02-24 17:12:27 -0500

    initial check

--- tdtrmv_gnu.sh
+++ tdtrmv_gnu.sh
@@ -1,8 +1,8 @@
 
 
 
-gfortran -Wall  -ffixed-form -D_OPENACC -o tdtrmv_acc lapack_acc.F90 lapack_mod.F90 tdtrmv.F90 -lblas
-gfortran -Wall  -ffixed-form -D_OPENMP=201511 -o tdtrmv_omp lapack_acc.F90 lapack_mod.F90 tdtrmv.F90 -lblas
-gfortran -Wall  -ffixed-form -U_OPENMP -U_OPENACC -o tdtrmv_serial lapack_acc.F90 lapack_mod.F90 tdtrmv.F90 -lblas
+gfortran -g -fbounds-check  -Wall  -ffixed-form -D_OPENACC -o tdtrmv_acc lapack_acc.F90 lapack_mod.F90 tdtrmv.F90 -lblas
+gfortran -g -fbounds-check  -Wall  -ffixed-form -D_OPENMP=201511 -o tdtrmv_omp lapack_acc.F90 lapack_mod.F90 tdtrmv.F90 -lblas
+gfortran -g -fbounds-check  -Wall  -ffixed-form -U_OPENMP -U_OPENACC -o tdtrmv_serial lapack_acc.F90 lapack_mod.F90 tdtrmv.F90 -lblas
 
 
